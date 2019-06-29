d27cd5b004f31afc5c7eb52c9ba83c7446b24597
thepokasoft <pokasoft9@gmail.com>
2019-06-28 22:58:02 -0400

    Simple working intend updated

--- index.js
+++ index.js
@@ -15,120 +15,91 @@ admin.initializeApp();
 
 // Handle the Dialogflow intent named 'favorite color'.
 // The intent collects a parameter named 'color'.
-// This is for practice and tutorial. 
 app.intent('favorite color', (conv, {color}) => {
     const luckyNumber = color.length;
     // Respond with the user's lucky number and end the conversation.
     conv.close('Your lucky number is ' + luckyNumber);
 });
 
-
-// Declare Database connection.
 const db = admin.firestore();
-const collectionRef = db.collection('timetable');
+//const collectionRef = db.collection('timetable');
+const usaTime = new Date(new Date().toLocaleString("en-US", {timeZone: "America/New_York"},{hour12: false}));
 
-// This intent takes the bus route and place as input. eg: DCL Leroy, WS Mainst, DCL Murray.
-// This intent is made as a POC to check the MODEL and response to the user.
-app.intent('find bus', (conv, {places,busroutes}) => {
-
-  var route = busroutes;
-  /*if(busroute.includes("dcl")){
-    route = 'DCL';
-  }else if(busroute.includes("ws")){
-    route = 'WS';
-  }*/
-  const collectionRef = db.collection(`${route}`);
-  
-  var usaTime = new Date().toLocaleString("en-US", {timeZone: "America/New_York"},{hour12: false});
-  usaTime = new Date(usaTime);
-  var h = usaTime.getHours();
-  var day = usaTime.getDay();
-  var week = "week";
-  if(day ===0 || day === 6)
-    week = "weekend";
-  var termRef = collectionRef.doc(`${week}`);
-	
-  return termRef.get()
-    .then((snapshot) => {
-    	if(snapshot.data()){
-      		const { inbound } = snapshot.data();
-          	var cm = usaTime.getMinutes();
-			var arrayLength = inbound.length;
-          	var text  = " ";
-          	var count = 0;
-          var diff =0;
-         	for (var i = 0; i < arrayLength; i++) {
-              if(parseInt(inbound[i].substring(0,3)) >= h){
-              	count++;
-                  if(parseInt(inbound[i].substring(0,3)) === h){
-                  diff = inbound[i].substring(3,5) - cm; 
-                  if(parseInt(diff) > 0)
-                    text = text+ diff+" and ";
-                  }else{
-                    diff = (60-cm)+parseInt(inbound[i].substring(3,5));
-                    if(parseInt(diff) > 0)
-                      text = text+ diff+" and ";
-                  }
-                   if(count ==3)
-                     break;
-            	}
-            }
-          text = text.trim().substring(0,(text.length - 5));
-          conv.close(" Your next bus is in "+text+" minutes");
-        }
-    }).catch((e) => {
-      console.log('error:', e);
-      conv.close('Sorry, try again and tell me another street name.');
-	});
- });
-
-// start of route_direction_location
-// This is a next version of the Intent. More modified query to have efficient calls to database.
-app.intent('route_direction_location', (conv, {places,busroutes,directions}) => {
-    var place = places;
-    var busroute = busroutes;
-    var direction = directions;
-	
-    // Calculate Current Time
-  	var usaTime = new Date().toLocaleString("en-US", {timeZone: "America/New_York"},{hour12: false});
-    usaTime = new Date(usaTime);
-    var h = usaTime.getHours()+''+usaTime.getMinutes()+'';
+// action for Spot
+app.intent('spotbus', (conv, {busroutes,directions}) => {
+    console.log("spotbus intent is invoked with " + busroutes + " and "+ directions);
+  	
+    var h = usaTime.getHours();
+    var m = usaTime.getMinutes();
+  	var hm = Number(h+""+m);
+	if (m < 10) {
+		hm = hm+'0';
+	}
     var day = usaTime.getDay();
-	
-    // Calculate week or weekend
-    var week = "week";
+    var week = "Week";
     if(day ===0 || day === 6)
-        week = "weekend";
- 
- // Create colleciton string
-    var collection_string = '/'+busroute+'/'+week+'/'+direction;
+        week = "Weekend";
+    console.log(" week is : " + week);
+  	console.log("this is " +busroutes + " for the "+ week );
+	console.log(" USA time h is "+h+" and min is :"+m +" hm :"+hm +" day is "+day);
+  	var collection_string = '/DCLWeekendOutbound';
+    console.log('Collection String : ' + collection_string);
     const collectionRef = db.collection(collection_string);
-      return collectionRef.where('time','>',h)
-      .limit(4)
-      .get()
-      .then((snapshot) => {
-          var result = snapshot.docs.map((doc) => doc.get('time')).join(',');
-          var minutes = result.split(',');
-          var minute = ' ';
-          for (var index = 0; index < minutes.length; ++index) {
-            if(minutes[index].substring(0,2) == (usaTime.getHours()+1)){
-              var a = parseInt(60-usaTime.getMinutes());
-              var b = parseInt(minutes[index].substring(2,4));
-              var c = a+b;
-              minute = minute+ c;
-              minute = minute+' ';
-            }else if(minutes[index].substring(0,2) == usaTime.getHours()){
-              minute = minute+ minutes[index].substring(2,4);
-              minute = minute+' ';
-            }
-          }
-          conv.close("you have bus starting from Union in "+minute);
-        }).catch((e) => {
-          console.log('error:', e);
-          conv.close('Sorry, try again and tell me another street name.');
-      });
-    });
-// end of route_direction_location
+ 	console.log('Collection Ref' + collectionRef);
+      return collectionRef
+	  .where('start','>',hm)
+	  .limit(2)
+	  .get()
+	  .then((snapshot) => {
+		// This is a qury snapshot : 
+		// https://firebase.google.com/docs/reference/js/firebase.firestore.QuerySnapshot
+		/*console.log( "snalshot docs: "+snapshot.docs );
+		console.log( "snalshot empty: "+snapshot.empty );
+		console.log( "snalshot metadata: "+snapshot.metadata );
+		console.log( "snalshot query: "+snapshot.query );
+		console.log( "snalshot size: "+snapshot.size );*/
+        var temp = snapshot.docs.map((doc) => { if (doc.get('all') !== true) return doc.get('start'); }).join(",");
+        console.log("temp: "+temp);
+        var result = temp.split(",");
+		var abc = '';
+ 		for(var i=0; i<result.length; i++){
+          console.log(":"+result[i]+" Length:"+result[i].length);
+		  if(result[i].length > 0){
+			var temph, tempm;
+			if(result[i] < 1000){
+				// Time is AM. Hours is single digit.
+				result[i] = result[i]+"";
+				tempm  = parseInt(result[i].substring(1,3));
+				if (tempm < 10 ){
+					abc = abc+result[i].substring(0,1)+" O clock and "+result[i].substring(1,2)+" minutes. and ";
+				}else{
+					abc = abc+result[i].substring(0,1)+" O clock and "+result[i].substring(1,3)+" minutes. and ";
+				}
+			}else {
+				console.log(result[i]);
+				result[i] = result[i]+"";
+				temph  = parseInt(result[i].substring(0,2));
+				tempm  = parseInt(result[i].substring(2,4));
+				console.log(temph+" "+tempm);
+				if( temph > 12 )
+					temph = temph - 12;
+				temph = temph+"";
+				abc = abc+temph+" O clock and "+tempm+" minutes. and ";
 
+			}           
+		  }
+        }
+		abc = abc.substring(0,(abc.length-4));
+        console.log('your Next bus is at'+ abc+' .Do you want more options ?');
+		conv.close( 'your Next bus is at'+ abc+' .Do you want more options ?' );
+        });
+	
+    // Global exception.
+    //console.log("I am  having some trouble. Please contact the developer Dhyanesh");
+    //conv.close("I am  having some trouble. Please contact the developer Dhyanesh");
+});
+// End of Spot_bus
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+
+// https://www.youtube.com/watch?v=7IkUgCLr5oA
